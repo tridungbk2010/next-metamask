c78d7e84c2e481f585a5de57a107808ec7e1bf56
dungho <dung.ho@onemount.com>
2021-11-26 22:10:05 +0700

    changed document

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,27 +1,23 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document';
+import { Html, Head, Main, NextScript } from 'next/document';
 
-class MyDocument extends Document {
-  render() {
-    return (
-      <Html>
-        <Head lang="en">
-          <link rel="icon" type="image/png" href="/favicon.png" />
-          {/* <link
-            href="https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap"
-            rel="stylesheet"
-          /> */}
-          {/* <link
-            href="https://fonts.googleapis.com/css2?family=Courier+Prime&display=swap"
-            rel="stylesheet"
-          /> */}
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
-  }
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <link rel="icon" type="image/png" href="/favicon.png" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap"
+          rel="stylesheet"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Courier+Prime&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
 }
-
-export default MyDocument;
